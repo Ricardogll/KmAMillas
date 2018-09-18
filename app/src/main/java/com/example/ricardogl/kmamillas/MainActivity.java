9c5a929befec410b2fec1e2802c8eee6d9573202
Ricardogll <rikilonga@gmail.com>
2018-09-18 13:38:55 +0200

    Afegim AboutActivity

--- app/src/main/java/com/example/ricardogl/kmamillas/MainActivity.java
+++ app/src/main/java/com/example/ricardogl/kmamillas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ricardogl.kmamillas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -42,5 +43,10 @@ public class MainActivity extends AppCompatActivity {
             String skm = Float.toString(km);
             edit_km.setText(skm);
         }
+    }
+
+    public void onClickAbout(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
